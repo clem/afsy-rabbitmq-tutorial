7aa31d172249b8823ae5f03e376bc6fcb1b97098
Clément Delmas <clement.delmas@gmail.com>
2015-05-25 16:38:02 +0200

    [DefaultController] Add missing use and remove unused index

--- src/Afsy/Bundle/TutorialBundle/Controller/DefaultController.php
+++ src/Afsy/Bundle/TutorialBundle/Controller/DefaultController.php
@@ -3,14 +3,10 @@
 namespace Afsy\Bundle\TutorialBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('AfsyTutorialBundle:Default:index.html.twig', array('name' => $name));
-    }
-
     /**
      *  Download action
      */
